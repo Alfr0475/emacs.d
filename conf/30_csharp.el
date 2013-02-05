b25679456cbc8937de4897f6a4e55ffc80df1d80
Seiki Koga <alfr0475@gmail.com>
2013-02-05 19:01:51 +0900

    original-styleの修正。 C#のウザい機能を無効にした。

--- conf/30_csharp.el
+++ conf/30_csharp.el
@@ -3,5 +3,9 @@
 (add-hook 'csharp-mode-hook
           '(lambda()
              (c-set-style "original-style")
-             (font-lock-add-magic-number) ; 数値の色を変更する
+             ;;(font-lock-add-magic-number) ; 数値の色を変更する
+
+             ;; csharp-insert-open-braceを無効に
+             ;; 中括弧を入力した時点で勝手に指定スタイルで括弧を書かれるのが嫌だ
+             (local-unset-key (kbd "{"))
              ))
