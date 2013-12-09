fbc14ff9e8ce289eab9a7eb0adc9e3f73d3d8e38
Alfr0475 <alfr0475@gmail.com>
2013-12-09 18:51:17 +0900

    auto-async-byte-compileからpopwinの設定を削除。

--- conf/60_auto_async_byte_compile.el
+++ conf/60_auto_async_byte_compile.el
@@ -3,9 +3,3 @@
 
 ;;(setq auto-async-byte-compile-exclude-file-regexp "") ; 自動コンパイルを無効にするファイル名の正規表現
 (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
-
-;; auto-async-byte-compileのバッファをpopwinでポップアップする
-;; (setq display-buffer-function 'popwin:display-buffer)
-
-;; (push '(" *auto-async-byte-compile*" :height 14 :position bottom :noselect t) popwin:special-display-config)
-;; (push '("*VC-log*" :height 10 :position bottom) popwin:special-display-config)
