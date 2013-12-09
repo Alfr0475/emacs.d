23f1e07391b169277d49e88d9b45e9ea7f481c70
Alfr0475 <alfr0475@gmail.com>
2013-12-09 18:51:44 +0900

    GoogleTranslateの導入。

---
+++ site-lisp/google-translate-20131124.1319/google-translate-autoloads.el
@@ -0,0 +1,18 @@
+;;; google-translate-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("google-translate-pkg.el" "google-translate.el")
+;;;;;;  (21157 36697 861000 0))
+
+;;;***
+
+(provide 'google-translate-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; google-translate-autoloads.el ends here
