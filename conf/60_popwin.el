8e9f50fc3de377edebb119fde6e1b15be522d79e
Alfr0475 <alfr0475@gmail.com>
2013-12-09 18:52:34 +0900

    popwinの設定を独立。HelmやGoogleTranslateもpopwinに対応。

---
+++ conf/60_popwin.el
@@ -0,0 +1,13 @@
+(require 'popwin)
+(setq display-buffer-function 'popwin:display-buffer)
+
+(setq popwin:popup-window-position 'bottom) ; ポップアップの位置を画面下に表示
+(setq helm-samewindow nil)                  ; helmのポップアップウィンドウ対応
+
+
+;; ポップアップバッファ一覧
+(setq popwin:special-display-config '(
+                                      (" *auto-async-byte-compile*" :height 14 :noselect t)
+                                      ("helm" :regexp t :height 0.4)
+                                      ("*Google Translate*" :noselect t)
+))
