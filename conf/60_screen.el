f15176138eaf997bb57e9eab60dc86bebd36858a
seikikoga <seikikoga@gamania.com>
2014-06-02 12:42:44 +0900

    ELScreenの呼び出し方が変更されたのでEmacs24と23以前で呼び出し方を変更

--- conf/60_screen.el
+++ conf/60_screen.el
@@ -1,4 +1,13 @@
-(load "elscreen" "ElScreen" t)
+;; APEL非依存版 (Emacs 24の場合)
+(when (>= emacs-major-version 24)
+  (elscreen-start))
+
+
+;; APEL依存版 (Emacs 23と同居させたい場合)
+(when (<= emacs-major-version 23)
+  (add-to-load-path "elisp/library/apel")
+  (require 'elscreen))
+
 
 ;; frame-titleにスクリーンの一覧を表示する
 (defun elscreen-frame-title-update ()
