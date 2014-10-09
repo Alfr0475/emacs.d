71c867cd5a1aeb711c6ecf3dd66b5c6801955359
Seiki koga <alfr0475@gmail.com>
2014-10-09 20:20:33 +0900

    OSとクリップボードを共有する

--- conf/05_yank.el
+++ conf/05_yank.el
@@ -7,3 +7,18 @@
 (setq browse-kill-ring-highlight-current-entry t) ; 現在選択中の kill-ring のハイライトする
 (setq browse-kill-ring-separator-face 'region) ; 区切り文字のフェイスを指定する
 (setq browse-kill-ring-maximum-display-length 100) ; 一覧で表示する文字数を指定する。 nil ならすべて表示される。
+
+;; MacOSとクリップボードを共有する
+(cond ((and run-darwin)
+       (defun copy-from-osx ()
+         (shell-command-to-string "pbpaste"))
+
+       (defun paste-to-osx (text &optional push)
+         (let ((process-connection-type nil))
+           (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+             (process-send-string proc text)
+             (process-send-eof proc))))
+
+       (setq interprogram-cut-function 'paste-to-osx)
+       (setq interprogram-paste-function 'copy-from-osx)
+))
