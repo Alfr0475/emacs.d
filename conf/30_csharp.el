c8b6c34841852ce59d94298bcf96aa9d2a98fe77
Seiki koga <alfr0475@gmail.com>
2015-06-18 15:59:52 +0900

    csharpの設定を追加

--- conf/30_csharp.el
+++ conf/30_csharp.el
@@ -1,4 +1,7 @@
 (require 'csharp-mode)
+(require 'omnisharp)
+
+(setq omnisharp-server-executable-path "/Users/skoga/work/sources/github/github.com/nosami/OmniSharpServer/OmniSharp/bin/Debug/OmniSharp.exe")
 
 (add-hook 'csharp-mode-hook
           '(lambda()
@@ -13,3 +16,19 @@
              (omnisharp-mode)
              (auto-complete-mode)
              ))
+
+(define-key omnisharp-mode-map "\C-c\C-s" 'omnisharp-start-omnisharp-server)
+(define-key omnisharp-mode-map "\M-/" 'omnisharp-auto-complete)
+(define-key omnisharp-mode-map "." 'omnisharp-add-dot-and-auto-complete)
+(define-key omnisharp-mode-map "\C-c\C-c" 'omnisharp-build-in-emacs)
+(define-key omnisharp-mode-map "\C-c\C-N" 'omnisharp-navigate-to-solution-member)
+(define-key omnisharp-mode-map "\C-c\C-n" 'omnisharp-navigate-to-current-file-member)
+(define-key omnisharp-mode-map "\C-c\C-f" 'omnisharp-navigate-to-solution-file)
+(define-key omnisharp-mode-map "\C-c\C-g" 'omnisharp-go-to-definition)
+(define-key omnisharp-mode-map "\C-c\C-r" 'omnisharp-rename)
+(define-key omnisharp-mode-map "\C-c\C-v" 'omnisharp-run-code-action-refactoring)
+(define-key omnisharp-mode-map "\C-c\C-o" 'omnisharp-auto-complete-overrides)
+(define-key omnisharp-mode-map "\C-c\C-u" 'omnisharp-helm-find-symbols)
+(define-key omnisharp-mode-map "\C-c\C-t\C-s" (lambda() (interactive) (omnisharp-unit-test "single")))
+(define-key omnisharp-mode-map "\C-c\C-t\C-r" (lambda() (interactive) (omnisharp-unit-test "fixture")))
+(define-key omnisharp-mode-map "\C-c\C-t\C-e" (lambda() (interactive) (omnisharp-unit-test "all")))
--- conf/60_omnisharp.el
+++
@@ -1,2 +0,0 @@
-(require 'omnisharp)
-(setq omnisharp-server-executable-path (expand-file-name "/Users/skoga/work/sources/github/github.com/nosami/OmniSharpServer/OmniSharp/bin/Debug/OmniSharp.exe"))
