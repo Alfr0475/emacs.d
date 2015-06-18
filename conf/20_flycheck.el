02b8bfd85a6f371079dcba05703822086f80573b
Seiki koga <alfr0475@gmail.com>
2015-06-18 15:46:23 +0900

    flycheckをpoptipに対応

--- conf/20_flycheck.el
+++ conf/20_flycheck.el
@@ -1,3 +1,11 @@
 (require 'flycheck)
+(require 'flycheck-pos-tip)
+
 (setq flycheck-check-syntax-automatically '(mode-enabled save idle-change))
 (setq flycheck-idle-change-delay 2)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(eval-after-load 'flycheck
+  '(custom-set-variables
+    '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
