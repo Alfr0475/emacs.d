48d07f20c8927193213aa21c09eb53b0217471d0
Seiki koga <alfr0475@gmail.com>
2016-12-22 12:28:03 +0900

    mode設定をuse-package化

--- conf.d/mac-ns/30_mode.el
+++ conf.d/mac-ns/30_mode.el
@@ -7,245 +7,304 @@
 ;;------------------------------------------------------------------------------
 ;; PHP
 ;;------------------------------------------------------------------------------
-;; hook
-(add-hook 'php-mode-hook
-          '(lambda ()
-             (c-set-style "c-style-php") ; オリジナルのスタイルを使用
-             (setq comment-start "// ")
-             (setq comment-end   "")
-             (setq comment-start-skip "// *")
+(use-package php-mode
+  :config
+  (add-hook 'php-mode-hook
+            '(lambda ()
+               (c-set-style "c-style-php") ; オリジナルのスタイルを使用
+               (setq comment-start "// ")
+               (setq comment-end   "")
+               (setq comment-start-skip "// *")
 
-             ;; imenuでAllMethodsを表示しない
-             ;; http://qiita.com/osamu2001/items/511b558e5280dbf2b218
-             (assq-delete-all
-              (car (assoc "All Methods" php-imenu-generic-expression))
-              php-imenu-generic-expression)
+               ;; imenuでAllMethodsを表示しない
+               ;; http://qiita.com/osamu2001/items/511b558e5280dbf2b218
+               (assq-delete-all
+                (car (assoc "All Methods" php-imenu-generic-expression))
+                php-imenu-generic-expression)
 
-             (rainbow-mode t)
-             ;; auto-completionのphp用設定
-             (require 'php-completion)
-             (php-completion-mode t)
-             ;;(define-key php-mode-map (kbd "\C-o") 'phpcmp-complete)
-             (when (require 'auto-complete nil t)
-               (make-variable-buffer-local 'ac-sources)
-               (add-to-list ac-sources '(
-                                         ac-source-php
-                                         ac-source-php-completion
-                                         ac-source-words-in-same-mode-buffers
-                                         ac-source-filename
-                                         ))
-               (auto-complete-mode t))
-             ))
+               (rainbow-mode t)
+               ;; auto-completionのphp用設定
+               (require 'php-completion)
+               (php-completion-mode t)
+               ;;(define-key php-mode-map (kbd "\C-o") 'phpcmp-complete)
+               (when (require 'auto-complete nil t)
+                 (make-variable-buffer-local 'ac-sources)
+                 (add-to-list ac-sources '(
+                                           ac-source-php
+                                           ac-source-php-completion
+                                           ac-source-words-in-same-mode-buffers
+                                           ac-source-filename
+                                           ))
+                 (auto-complete-mode t))
+               )))
 
 ;;------------------------------------------------------------------------------
 ;; Ruby
 ;;------------------------------------------------------------------------------
 ;; RSense
-(cond ((and run-linux)
-       (setq rsense-home (expand-file-name "~/contrib/rsense/"))
-       (require 'rsense)
-       (setq rsense-rurema-home "~/data/rurema/ruby-refm-1.9.3-dynamic-20120829") ; リファレンスパスの指定
-))
+;; (cond ((and run-linux)
+;;        (setq rsense-home (expand-file-name "~/contrib/rsense/"))
+;;        (require 'rsense)
+;;        (setq rsense-rurema-home "~/data/rurema/ruby-refm-1.9.3-dynamic-20120829") ; リファレンスパスの指定
+;; ))
 
-(add-hook 'ruby-mode-hook
-          '(lambda ()
-             (setq tab-width 2)                 ; タブサイズ
-             (setq ruby-indent-level tab-width) ; インデントサイズ
-             (setq ruby-deep-indent-paren-style nil) ; 引数とかを改行して並べた時にインデントを括弧に合わせない
+;; (add-hook 'ruby-mode-hook
+;;           '(lambda ()
+;;              (setq tab-width 2)                 ; タブサイズ
+;;              (setq ruby-indent-level tab-width) ; インデントサイズ
+;;              (setq ruby-deep-indent-paren-style nil) ; 引数とかを改行して並べた時にインデントを括弧に合わせない
 
-             (cond ((and run-linux)
-                    (add-to-list 'ac-sources 'ac-source-rsense-method)
-                    (add-to-list 'ac-sources 'ac-source-rsense-constant)
-                    ))
-             ))
+;;              (cond ((and run-linux)
+;;                     (add-to-list 'ac-sources 'ac-source-rsense-method)
+;;                     (add-to-list 'ac-sources 'ac-source-rsense-constant)
+;;                     ))
+;;              ))
 
-;; ruby-block
-(require 'ruby-block)
-(ruby-block-mode t)
-(setq ruby-block-highlight-toggle t)    ; ミニバッファに表示し、かつオーバレイする
+;; ;; ruby-block
+;; (require 'ruby-block)
+;; (ruby-block-mode t)
+;; (setq ruby-block-highlight-toggle t)    ; ミニバッファに表示し、かつオーバレイする
 
 ;;------------------------------------------------------------------------------
 ;; JavaScript
 ;;------------------------------------------------------------------------------
-(when (load "js2" t)
-  (require 'js)
-  (setq js2-bounce-indent-flag nil
-        js2-mirror-mode nil
-        )
+;; (when (load "js2" t)
+;;   (require 'js)
+;;   (setq js2-bounce-indent-flag nil
+;;         js2-mirror-mode nil
+;;         )
 
-  ;; インデントしたときにスペースを除いた行頭にポイントが移動
-  (defun indent-and-back-to-indentation ()
-    (interactive)
-    (indent-for-tab-command)
-    (let ((point-of-indentation
-           (save-excursion
-             (back-to-indentation)
-             (point))))
-      (skip-chars-forward "\s " point-of-indentation)))
+;;   ;; インデントしたときにスペースを除いた行頭にポイントが移動
+;;   (defun indent-and-back-to-indentation ()
+;;     (interactive)
+;;     (indent-for-tab-command)
+;;     (let ((point-of-indentation
+;;            (save-excursion
+;;              (back-to-indentation)
+;;              (point))))
+;;       (skip-chars-forward "\s " point-of-indentation)))
 
-  (define-key js2-mode-map "\C-i" 'indent-and-back-to-indentation)
-  (define-key js2-mode-map "\C-m" 'newline-and-indent)
+;;   (define-key js2-mode-map "\C-i" 'indent-and-back-to-indentation)
+;;   (define-key js2-mode-map "\C-m" 'newline-and-indent)
 
-  ;; ejacs設定
-  (autoload 'js-console "js-console" nil t)
-  (defun js-console-execute-region (start end)
-    "Execute region"
-    (interactive "r")
-    (let ((buf-name (buffer-name (current-buffer))))
-      (copy-region-as-kill start end)
-      (switch-to-buffer-other-window "*js*")
-      (js-console-exec-input (car kill-ring))
-      (switch-to-buffer-other-window buf-name)))
-  (defun run-js-console-and-split-window ()
-    "Run js-console and split window horizontally."
-    (interactive)
-    (split-window-horizontally)
-    (js-console)
-    (other-window 1)
-    )
+;;   ;; ejacs設定
+;;   (autoload 'js-console "js-console" nil t)
+;;   (defun js-console-execute-region (start end)
+;;     "Execute region"
+;;     (interactive "r")
+;;     (let ((buf-name (buffer-name (current-buffer))))
+;;       (copy-region-as-kill start end)
+;;       (switch-to-buffer-other-window "*js*")
+;;       (js-console-exec-input (car kill-ring))
+;;       (switch-to-buffer-other-window buf-name)))
+;;   (defun run-js-console-and-split-window ()
+;;     "Run js-console and split window horizontally."
+;;     (interactive)
+;;     (split-window-horizontally)
+;;     (js-console)
+;;     (other-window 1)
+;;     )
 
-  ;; js-modeのタブ設定を利用する
-  (add-hook 'js2-mode-hook
-            '(lambda ()
-               (require 'js)
-               (setq js-indent-level 4
-                     js-expr-indent-offset 4
-                     indent-tabs-mode nil
-                     )
+;;   ;; js-modeのタブ設定を利用する
+;;   (add-hook 'js2-mode-hook
+;;             '(lambda ()
+;;                (require 'js)
+;;                (setq js-indent-level 4
+;;                      js-expr-indent-offset 4
+;;                      indent-tabs-mode nil
+;;                      )
 
-               (moz-minor-mode 1) ;; 要moz.el
-               (local-set-key "\C-c\C-j" 'run-js-console-and-split-window)
-               (local-set-key "\C-cr" 'js-console-execute-region)
+;;                (moz-minor-mode 1) ;; 要moz.el
+;;                (local-set-key "\C-c\C-j" 'run-js-console-and-split-window)
+;;                (local-set-key "\C-cr" 'js-console-execute-region)
 
-               ;; case用のインデント関数
-               (defun my-js-indent-line ()
-                 (interactive)
-                 (let* ((parse-status (save-excursion (syntax-ppss (point-at-bol))))
-                        (offset (- (current-column) (current-indentation)))
-                        (indentation (js--proper-indentation parse-status)))
-                   (back-to-indentation)
-                   (if (looking-at "case\\s-")
-                       (indent-line-to (+ indentation 2))
-                     (js-indent-line))
-                   (when (> offset 0) (forward-char offset))))
-               (set (make-local-variable 'indent-line-function) 'my-js-indent-line)))
+;;                ;; case用のインデント関数
+;;                (defun my-js-indent-line ()
+;;                  (interactive)
+;;                  (let* ((parse-status (save-excursion (syntax-ppss (point-at-bol))))
+;;                         (offset (- (current-column) (current-indentation)))
+;;                         (indentation (js--proper-indentation parse-status)))
+;;                    (back-to-indentation)
+;;                    (if (looking-at "case\\s-")
+;;                        (indent-line-to (+ indentation 2))
+;;                      (js-indent-line))
+;;                    (when (> offset 0) (forward-char offset))))
+;;                (set (make-local-variable 'indent-line-function) 'my-js-indent-line)))
 
-  )
+;;   )
 
 ;;------------------------------------------------------------------------------
 ;; C#
 ;;------------------------------------------------------------------------------
-(require 'csharp-mode)
-;;(require 'omnisharp)
+;; (require 'csharp-mode)
+;; ;;(require 'omnisharp)
 
-(setq omnisharp-server-executable-path (expand-file-name "/Users/skoga/work/sources/git/github.com/OmniSharp/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe"))
+;; (setq omnisharp-server-executable-path (expand-file-name "/Users/skoga/work/sources/git/github.com/OmniSharp/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe"))
 
-(add-hook 'csharp-mode-hook
-          '(lambda()
-             (c-set-style "c-style-base") ; オリジナルのスタイルを使用
-             ;;(set-original-c-style-base)
-             ;;(font-lock-add-magic-number) ; 数値の色を変更する
+;; (add-hook 'csharp-mode-hook
+;;           '(lambda()
+;;              (c-set-style "c-style-base") ; オリジナルのスタイルを使用
+;;              ;;(set-original-c-style-base)
+;;              ;;(font-lock-add-magic-number) ; 数値の色を変更する
 
-             ;; csharp-insert-open-braceを無効に
-             ;; 中括弧を入力した時点で勝手に指定スタイルで括弧を書かれるのが嫌だ
-             (local-unset-key (kbd "{"))
+;;              ;; csharp-insert-open-braceを無効に
+;;              ;; 中括弧を入力した時点で勝手に指定スタイルで括弧を書かれるのが嫌だ
+;;              (local-unset-key (kbd "{"))
 
-             (flycheck-mode 1)
-             (omnisharp-mode)
-             (auto-complete-mode)
+;;              (flycheck-mode 1)
+;;              (omnisharp-mode)
+;;              (auto-complete-mode)
 
-             (define-key omnisharp-mode-map "\C-c\C-s" 'omnisharp-start-omnisharp-server)
-             (define-key omnisharp-mode-map "\M-/" 'omnisharp-auto-complete)
-             (define-key omnisharp-mode-map "." 'omnisharp-add-dot-and-auto-complete)
-             (define-key omnisharp-mode-map "\C-c\C-c" 'omnisharp-build-in-emacs)
-             (define-key omnisharp-mode-map "\C-c\C-N" 'omnisharp-navigate-to-solution-member)
-             (define-key omnisharp-mode-map "\C-c\C-n" 'omnisharp-navigate-to-current-file-member)
-             (define-key omnisharp-mode-map "\C-c\C-f" 'omnisharp-navigate-to-solution-file)
-             (define-key omnisharp-mode-map "\C-c\C-g" 'omnisharp-go-to-definition)
-             (define-key omnisharp-mode-map "\C-c\C-r" 'omnisharp-rename)
-             (define-key omnisharp-mode-map "\C-c\C-v" 'omnisharp-run-code-action-refactoring)
-             (define-key omnisharp-mode-map "\C-c\C-o" 'omnisharp-auto-complete-overrides)
-             (define-key omnisharp-mode-map "\C-c\C-u" 'omnisharp-helm-find-symbols)
-             (define-key omnisharp-mode-map "\C-c\C-t\C-s" (lambda() (interactive) (omnisharp-unit-test "single")))
-             (define-key omnisharp-mode-map "\C-c\C-t\C-r" (lambda() (interactive) (omnisharp-unit-test "fixture")))
-             (define-key omnisharp-mode-map "\C-c\C-t\C-e" (lambda() (interactive) (omnisharp-unit-test "all")))
-             ))
+;;              (define-key omnisharp-mode-map "\C-c\C-s" 'omnisharp-start-omnisharp-server)
+;;              (define-key omnisharp-mode-map "\M-/" 'omnisharp-auto-complete)
+;;              (define-key omnisharp-mode-map "." 'omnisharp-add-dot-and-auto-complete)
+;;              (define-key omnisharp-mode-map "\C-c\C-c" 'omnisharp-build-in-emacs)
+;;              (define-key omnisharp-mode-map "\C-c\C-N" 'omnisharp-navigate-to-solution-member)
+;;              (define-key omnisharp-mode-map "\C-c\C-n" 'omnisharp-navigate-to-current-file-member)
+;;              (define-key omnisharp-mode-map "\C-c\C-f" 'omnisharp-navigate-to-solution-file)
+;;              (define-key omnisharp-mode-map "\C-c\C-g" 'omnisharp-go-to-definition)
+;;              (define-key omnisharp-mode-map "\C-c\C-r" 'omnisharp-rename)
+;;              (define-key omnisharp-mode-map "\C-c\C-v" 'omnisharp-run-code-action-refactoring)
+;;              (define-key omnisharp-mode-map "\C-c\C-o" 'omnisharp-auto-complete-overrides)
+;;              (define-key omnisharp-mode-map "\C-c\C-u" 'omnisharp-helm-find-symbols)
+;;              (define-key omnisharp-mode-map "\C-c\C-t\C-s" (lambda() (interactive) (omnisharp-unit-test "single")))
+;;              (define-key omnisharp-mode-map "\C-c\C-t\C-r" (lambda() (interactive) (omnisharp-unit-test "fixture")))
+;;              (define-key omnisharp-mode-map "\C-c\C-t\C-e" (lambda() (interactive) (omnisharp-unit-test "all")))
+;;              ))
 
 ;;------------------------------------------------------------------------------
 ;; web-mode
 ;;------------------------------------------------------------------------------
-(require 'web-mode)
-(require 'ac-html)
+(use-package web-mode
+  :mode (
+         ("\\.html$" . web-mode)
+         ("\\.tpl$" . web-mode)
+         ("\\.ctp$" . web-mode)
+         ("\\.blade\\.php$" . web-mode)
+         )
+  :config
+  (require 'ac-html)
+  (add-hook 'web-mode-hook
+            '(lambda ()
+               (add-to-list 'ac-modes 'web-mode)
 
-(defun web-mode-hook ()
-  (add-to-list 'ac-modes 'web-mode)
+               (setq web-mode-markup-indent-offset 4) ; HTML
+               (setq web-mode-css-indent-offset 4)    ; CSS
+               (setq web-mode-code-indent-offset 4)   ; JavaScript,PHP
 
-  (setq web-mode-markup-indent-offset 4) ; HTML
-  (setq web-mode-css-indent-offset 4)    ; CSS
-  (setq web-mode-code-indent-offset 4)   ; JavaScript,PHP
+               (setq web-mode-style-padding 4)       ; <style>
+               (setq web-mode-script-padding 4)      ; <script>
 
-  (setq web-mode-style-padding 4)       ; <style>
-  (setq web-mode-script-padding 4)      ; <script>
+               (setq web-mode-enable-auto-pairing nil) ; 自動タグ閉じ
+               (setq web-mode-enable-css-colorization nil) ; RGBをバックグラウンドに反映
 
-  (setq web-mode-enable-auto-pairing nil) ; 自動タグ閉じ
-  (setq web-mode-enable-css-colorization nil) ; RGBをバックグラウンドに反映
+               (setq indent-tabs-mode t)
 
-  (setq indent-tabs-mode t)
+               (rainbow-mode t)
 
-  (rainbow-mode t)
-
-  (setq web-mode-ac-sources-alist
-        '(("css" . (ac-source-css-property))
-          ("html" . (ac-source-html-attribute-value
-                     ac-source-html-tag
-                     ac-source-html-attribute
-                     ))
-          ("php" . (ac-source-php-completion
-                    ac-source-words-in-same-mode-buffers
-                    ac-source-filename))
-          )
-        )
-  )
-(add-hook 'web-mode-hook 'web-mode-hook)
+               (setq web-mode-ac-sources-alist
+                     '(("css" . (ac-source-css-property))
+                       ("html" . (ac-source-html-attribute-value
+                                  ac-source-html-tag
+                                  ac-source-html-attribute
+                                  ))
+                       ("php" . (ac-source-php-completion
+                                 ac-source-words-in-same-mode-buffers
+                                 ac-source-filename))
+                       )
+                     )
+               )))
 
 ;;------------------------------------------------------------------------------
 ;; Shell Script
 ;;------------------------------------------------------------------------------
-(add-hook 'sh-mode-hook
-          '(lambda ()
-             (c-set-style "original-style")
-             (auto-complete-mode t)
-             ))
+(use-package sh-mode
+  :mode (
+         ("\\.bash$" . sh-mode)
+         ("\\.zsh$" . sh-mode)
+         ("zsh\\.d\\/zsh[env|function|rc]" . sh-mode)
+         )
+  :config
+  (add-hook 'sh-mode-hook
+            '(lambda ()
+               (c-set-style "original-style")
+               (auto-complete-mode t)
+               )))
 
 ;;------------------------------------------------------------------------------
 ;; batch
 ;;------------------------------------------------------------------------------
-(require 'cmd-mode)
+(use-package cmd-mode
+  :mode (
+         ("\\.[bB][aA][tT]$" . cmd-mode)
+         ))
 
 ;;------------------------------------------------------------------------------
 ;; markdown
 ;;------------------------------------------------------------------------------
-(require 'markdown-mode)
+(use-package markdown-mode
+  :mode (
+         ("\\.md$" . gfm-mode)
+         ))
 
 ;;------------------------------------------------------------------------------
 ;; textile
 ;;------------------------------------------------------------------------------
-(require 'textile-mode)
+(use-package textile-mode
+  :mode (
+         ("\\.textile$" . textile-mode)
+         ))
 
 ;;------------------------------------------------------------------------------
 ;; yaml
 ;;------------------------------------------------------------------------------
-(require 'yaml-mode)
+(use-package yaml-mode
+  :mode (
+         ("\\.yml$" . yaml-mode)
+         ))
 
 ;;------------------------------------------------------------------------------
-;; css
+;; json
 ;;------------------------------------------------------------------------------
-(add-hook 'css-mode-hook 'rainbow-mode)
+(use-package json-mode
+  :mode (
+         ("\\.json$" . json-mode)
+         ))
 
 ;;------------------------------------------------------------------------------
-;; scss
+;; csv, tsv
 ;;------------------------------------------------------------------------------
-(add-hook 'scss-mode-hook 'rainbow-mode)
+(use-package csv-mode
+  :mode (
+         ("\\.csv$" . csv-mode)
+         ))
+
+(use-package tsv-mode
+  :mode (
+         ("\\.tsv$" . tsv-mode)
+         ))
+
+;;------------------------------------------------------------------------------
+;; css, scss, sass
+;;------------------------------------------------------------------------------
+(use-package css-mode
+  :mode (
+         ("\\.css$" . css-mode)
+         )
+  :config
+  (add-hook 'css-mode-hook 'rainbow-mode))
+
+(use-package scss-mode
+  :mode (
+         ("\\.scss$" . scss-mode)
+         )
+  :config
+  (add-hook 'scss-mode-hook 'rainbow-mode))
+
+(use-package sass-mode
+  :mode (
+         ("\\.sass$" . sass-mode)
+         ))
 
 ;;------------------------------------------------------------------------------
 ;; org
--- conf.d/mac-ns/90_association.el
+++ conf.d/mac-ns/90_association.el
@@ -1,46 +1,8 @@
 ;; JavaScript
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-;; HTML
-(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.ctp$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.blade\\.php$" . web-mode))
-
-;; Shell Script
-(add-to-list 'auto-mode-alist '("\\.bash$" . sh-mode))
-(add-to-list 'auto-mode-alist '("\\.zsh$" . sh-mode))
-(add-to-list 'auto-mode-alist '("zsh\\.d\\/zsh[env|function|rc]" . sh-mode))
-
-;; batch
-(add-to-list 'auto-mode-alist '("\\.[bB][aA][tT]$" . cmd-mode))
-
-;; markdown
-(add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
-
-;; textile
-(add-to-list 'auto-mode-alist '("\\.textile$" . textile-mode))
-
-;; yaml
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-
 ;; org
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-
-;; csv
-(add-to-list 'auto-mode-alist '("\\.csv$" . csv-mode))
-
-;; json
-(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
-
-;; sass
-(add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
-
-;; scss
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
-
-;; tsv
-(add-to-list 'auto-mode-alist '("\\.tsv$" . tsv-mode))
 
 ;; ssh config
 (add-to-list 'auto-mode-alist '("\\.ssh\\/config$" . ssh-config-mode))
