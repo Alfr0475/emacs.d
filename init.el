c99771fef256796b48f30313f0aaee395c7557bb
Seiki koga <alfr0475@gmail.com>
2017-02-17 16:47:40 +0900

    web-modeの設定変更

--- init.el
+++ init.el
@@ -866,36 +866,38 @@
            ("\\.blade\\.php$" . web-mode)
            )
     :config
-    (require 'ac-html)
     (add-hook 'web-mode-hook
               '(lambda ()
-                 (add-to-list 'ac-modes 'web-mode)
+                 (setq web-mode-markup-indent-offset 2) ; HTML
+                 (setq web-mode-css-indent-offset 2)    ; CSS
+                 (setq web-mode-code-indent-offset 2)   ; JavaScript,PHP
+                 (setq web-mode-attr-indent-offset 2)   ; attr改行時のインデント
 
-                 (setq web-mode-markup-indent-offset 4) ; HTML
-                 (setq web-mode-css-indent-offset 4)    ; CSS
-                 (setq web-mode-code-indent-offset 4)   ; JavaScript,PHP
-
-                 (setq web-mode-style-padding 4)       ; <style>
-                 (setq web-mode-script-padding 4)      ; <script>
+                 (setq web-mode-style-padding 2)       ; <style>
+                 (setq web-mode-script-padding 2)      ; <script>
 
                  (setq web-mode-enable-auto-pairing nil) ; 自動タグ閉じ
                  (setq web-mode-enable-css-colorization nil) ; RGBをバックグラウンドに反映
 
-                 (setq indent-tabs-mode t)
+                 (setq indent-tabs-mode nil)
+
+                 (setq web-mode-engines-alist
+                       '(("blade" . "\\.blade\\."))
+                       )
 
                  (rainbow-mode t)
 
-                 (setq web-mode-ac-sources-alist
-                       '(("css" . (ac-source-css-property))
-                         ("html" . (ac-source-html-attribute-value
-                                    ac-source-html-tag
-                                    ac-source-html-attribute
-                                    ))
-                         ("php" . (
-                                   ac-source-words-in-same-mode-buffers
-                                   ac-source-filename))
-                         )
-                       )
+                 ;; (setq web-mode-ac-sources-alist
+                 ;;       '(("css" . (ac-source-css-property))
+                 ;;         ("html" . (ac-source-html-attribute-value
+                 ;;                    ac-source-html-tag
+                 ;;                    ac-source-html-attribute
+                 ;;                    ))
+                 ;;         ("php" . (
+                 ;;                   ac-source-words-in-same-mode-buffers
+                 ;;                   ac-source-filename))
+                 ;;         )
+                 ;;       )
                  )))
   )
 
